ddc00e6ae39624ef25cad6c343b6a20181c634f5
Ksandur <35775481+Ksandur@users.noreply.github.com>
2020-09-22 11:34:43 +0200

    Another small update

--- commands/maxim.js
+++ commands/maxim.js
@@ -5,8 +5,8 @@ module.exports = {
 	execute(message, args) {
         var maximMessage = new Discord.RichEmbed()
         .setTitle("Maxim Mode")
-        .addField("сука блять")
-        .addField("ARE YOU GQY :OTHQRFUCKER")
+        .addField("сука блять", "Иди нахуй")
+        .addField("ARE YOU GQY :OTHQRFUCKER", "I FOKEDD UR MOM NGDANLK")
         .setColor(0x3399FF)
         .setTimestamp()
         message.channel.send(maximMessage);
